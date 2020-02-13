d2465a454f3f5f55d2d2c9a418e178d8a5ce6a91
sSeongJae91 <ben91429@gmail.com>
2020-02-13 15:35:47 +0000

    python parctice 1. variable 2. condition

---
+++ condition.py
@@ -0,0 +1,15 @@
+if not 1>2:
+  print("hello")
+
+if 1>0 and 2>1:
+  print("hello2")
+
+
+x = 3
+
+if x > 5:
+  print("hello3")
+elif x == 3:
+  print("hello4")
+else:
+  print("hello5")
--- variable.py
+++ variable.py
@@ -1,1 +1,17 @@
-  
+#casting
+print("Hello World : " + str(1))
+x = "1"
+
+z = """
+안녕하세요
+정성재입니다.
+"""
+### 문자열 곱하기
+print(z)
+print(x * 2)
+
+#boolean
+i = True
+j = False
+print(i)
+print(j)
