04e7761fa51fb27b387ff7f3ce92b44febf9d65f
sSeongjae91 <ben91429@gmail.com>
2020-11-22 00:47:30 +0900

    python practice

---
+++ animal/__init__.py
@@ -0,0 +1,2 @@
+from .cat import Cat #현재 디렉토리 경로의 cat.py에서 Cat class를 import
+from .dog import Dog
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#practice
+#package = 라이브러리, module들의 합
+
+#module = 코드가 들어가있는 파일
+
+#animal package
+#dog, cat module
+
+from animal import dog #animal 패키지에서 dog라는 모듈을 import
+from animal import cat
+
+from animal import * #모든 모듈을 import
+
+d = dog.Dog() #instance
+d.hi()
+
+c = cat.Cat()
+c.hi()
+
+
+# *로 전체 import 했을 시
+e = Dog()
+e.hi()
+
+f = Cat()
+f.hi()
